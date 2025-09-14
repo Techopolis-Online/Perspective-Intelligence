283e53cdac0cf7d6c1b529a463f183609ac186c9
Michael Doise <mikedoise@icloud.com>
2025-09-14 13:53:14 -0500

    Initial Commit

---
+++ Perspective Intelligence/Perspective_IntelligenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Perspective_IntelligenceApp.swift
+//  Perspective Intelligence
+//
+//  Created by Michael Doise on 9/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Perspective_IntelligenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
